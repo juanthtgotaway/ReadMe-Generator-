59da26060291b2594949ab1bb7d6243395f6700c
Juan Acevedo <jmamartinez18@gmail.com>
2023-12-16 16:44:07 -0700

    added tests

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,13 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
 // function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
@@ -20,16 +20,19 @@ ${data.Description}
 *[Installation](#Installation)
 *[Usage](#Usage)
 *[Credits](#Credits)
+*[Tests](#Tests)
 ##Installation
 ${data.Installation}
 ##Usage
 ${data.Usage}
 ##Credits
 ${data.Credits}
+##Tests
+${data.Tests}
 ##License
 ${data.License}
 ##Questions
-If you have any questons or concerns feel free to reach out via the following:
+If you have any questions or concerns feel free to reach out via the following:
 ${data.Username}
 ${data.Email}
 
